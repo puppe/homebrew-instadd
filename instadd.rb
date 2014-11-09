a37a641cb6c131bc0652835d39798fc62beb181d
Martin Puppe <mail@mpuppe.de>
2014-11-09 23:16:37 +0100

    Set correct sha1

--- instadd.rb
+++ instadd.rb
@@ -3,7 +3,7 @@ require "formula"
 class Instadd < Formula
   homepage "https://github.com/puppe/instadd"
   url "https://github.com/puppe/instadd/archive/1.0.0.tar.gz"
-  sha1 "7c6a308b7d81b98d6f6043e555b176ceb6dbb322"
+  sha1 "66f94ac74d9d0891058f414a03621dca3f158c2a"
 
   def install
       bin.install "instadd"
