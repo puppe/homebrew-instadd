ce8b24449e7aca76b5e48531217ccb61c1663704
Martin Puppe <mp@mpuppe.de>
2016-08-03 17:50:28 +0200

    Use sha256 instead of sha1

--- instadd.rb
+++ instadd.rb
@@ -3,7 +3,7 @@ require "formula"
 class Instadd < Formula
   homepage "https://github.com/puppe/instadd"
   url "https://github.com/puppe/instadd/archive/1.0.1.tar.gz"
-  sha1 "ce4f122d5c779d4b5588b290af61c45c6eb80177"
+  sha256 "26ac6846b3ed0a453ac0fe7df5f7e8ed699605024d18b2e16efea8342f3209a3"
 
   def install
       bin.install "instadd"
